2745b13e251ed03915523320dc590c0742e86202
Irina <irik.pruglo@mail.ru>
2022-11-07 17:28:35 +0100

    add component to load more characters

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,7 @@ class MarvelService {
 
     _apiBase = "https://gateway.marvel.com:443/v1/public/";
     _apiKey = "apikey=79bdfc05b5558c65076634caca17d8e8";
+    _baseOffset = 210;
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -13,20 +14,23 @@ class MarvelService {
         return await (res.json());
     }
 
-    getAllCharacters = async() => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async(offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
     }
-    getOneCharacters = async(id) => {
+    getOneCharacter = async(id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
         return this._transformCharacter(res.data.results[0]);
     }
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
-            description: char.description,
+            description: (char.description) ? `${char.description.slice(0, 210)}...` : "There is no description for this character",
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension ,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
+            comics: char.comics.items,
         }
     }
 }
